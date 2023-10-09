ceb1805b06c2ebb4c6db4537aca60b1fe751b1aa
Melany Mendoza Vargas <melanymendoza2001@gmail.com>
2023-10-08 23:30:27 -0600

    loans

--- src/controller/FXMLDashboardAdminController.java
+++ src/controller/FXMLDashboardAdminController.java
@@ -78,5 +78,13 @@ public class FXMLDashboardAdminController implements Initializable {
         containerMenu.getChildren().add(root);
     }
 
+    @FXML
+    private void openLoans(MouseEvent event) throws IOException {
+        System.out.println("clicked me!");
+        Parent root = FXMLLoader.load(getClass().getResource("/vista/FXMLLoans.fxml"));
+        containerMenu.getChildren().clear(); //lipiar
+        containerMenu.getChildren().add(root);
+    }
+
     
 }
