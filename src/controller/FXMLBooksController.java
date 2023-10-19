31851d5937c06c8f41fa78377cad6dc379bb3878
AntonyValverde <gatovalverde4@gmail.com>
2023-10-19 14:34:01 -0600

    Book_Validation

--- src/controller/FXMLBooksController.java
+++ src/controller/FXMLBooksController.java
@@ -12,6 +12,7 @@ import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -77,12 +78,11 @@ public class FXMLBooksController implements Initializable {
     private ObservableList<Libro> Libro = FXCollections.observableArrayList();
 
     private ObservableList<Libro> Libros;
-
+    String idAux;
     Integer index;
     Connection conn = null;
-    PreparedStatement ps = null;
+    PreparedStatement ps, ps1 = null;
     ResultSet rs = null;
-    
 
     /**
      * Initializes the controller class.
@@ -93,35 +93,73 @@ public class FXMLBooksController implements Initializable {
         cargarDatos();
     }
 
+    public String validation() throws SQLException {
+    conn = ConexionLibros.conn();
+
+    String valueAux = txt_isbn.getText();
+    String sqlAux = "SELECT DISTINCT isbn FROM book WHERE isbn = ?";
+    ps1 = conn.prepareStatement(sqlAux);
+    ps1.setString(1, valueAux);
+
+    ResultSet resultSet = ps1.executeQuery();
+
+    if (resultSet.next()) {
+         
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText(null);
+        alert.setTitle("INFORMACIÓN");
+        alert.setContentText("El libro ya se encuentra en la biblioteca, coloca distinto Código.");
+        alert.showAndWait();
+
+        resultSet.close();
+        ps1.close();
+        conn.close();
+        idAux = "1";
+        return idAux;
+    } else {
+        System.out.println("No se encontraron registros que cumplan con la condición.");
+
+        resultSet.close();
+        ps1.close();
+        conn.close();
+        idAux = "0";
+        return idAux;
+    }
+}
+
+
     @FXML
-    private void add(ActionEvent event) {
+    private void add(ActionEvent event) throws SQLException {
+        validation();
         conn = ConexionLibros.conn();
+        if (idAux == "0") {
+            String sql = "insert into book(isbn, title, authorBook,"
+                    + " editorial, available, releaseDate)values(?,?,?,?,?,?)";
 
-        String sql = "insert into book(isbn, title, authorBook,"
-                + " editorial, available, releaseDate)values(?,?,?,?,?,?)";
+            try {
+                ps = conn.prepareStatement(sql);
+                ps.setString(1, txt_isbn.getText());
+                ps.setString(2, txt_title.getText());
+                ps.setString(3, txt_author.getText());
+                ps.setString(4, txt_editorial.getText());
+                ps.setString(5, "Disponible");
+                ps.setString(6, datePicker.getValue().toString());
+                ps.execute();
 
-        try {
-            ps = conn.prepareStatement(sql);
-            ps.setString(1, txt_isbn.getText());
-            ps.setString(2, txt_title.getText());
-            ps.setString(3, txt_author.getText());
-            ps.setString(4, txt_editorial.getText());
-            ps.setString(5, "Disponible");
-            ps.setString(6, datePicker.getValue().toString());
-            ps.execute();
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setHeaderText(null);
+                alert.setTitle("INFORMACIÓN");
+                alert.setContentText("Libros guardados correctamente.");
+                alert.showAndWait();
 
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setHeaderText(null);
-            alert.setTitle("INFORMACIÓN");
-            alert.setContentText("Libros guardados correctamente.");
-            alert.showAndWait();
+            } catch (Exception e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText(null);
+                alert.setTitle("ERROR");
+                alert.setContentText("No se pudo guardar los libros. " + e);
+                alert.showAndWait();
+            }
 
-        } catch (Exception e) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setHeaderText(null);
-            alert.setTitle("ERROR");
-            alert.setContentText("No se pudo guardar los libros. " + e);
-            alert.showAndWait();
         }
         cargarDatos();
         limpiarDatos();
@@ -215,18 +253,12 @@ public class FXMLBooksController implements Initializable {
     }
 
     private void cargarDatos() {
-        this.column_isbn.setCellValueFactory(new PropertyValueFactory<Libro,
-                Integer>("isbn"));
-        this.column_title.setCellValueFactory(new PropertyValueFactory<Libro,
-                String>("title"));
-        this.column_author.setCellValueFactory(new PropertyValueFactory<Libro,
-                String>("authorBook"));
-        this.column_editorial.setCellValueFactory(new PropertyValueFactory<Libro,
-                String>("editorial"));
-        this.column_year.setCellValueFactory(new PropertyValueFactory<Libro,
-                java.sql.Date>("releaseDate"));
-        this.column_available.setCellValueFactory(new PropertyValueFactory<Libro,
-                String>("available"));
+        this.column_isbn.setCellValueFactory(new PropertyValueFactory<Libro, Integer>("isbn"));
+        this.column_title.setCellValueFactory(new PropertyValueFactory<Libro, String>("title"));
+        this.column_author.setCellValueFactory(new PropertyValueFactory<Libro, String>("authorBook"));
+        this.column_editorial.setCellValueFactory(new PropertyValueFactory<Libro, String>("editorial"));
+        this.column_year.setCellValueFactory(new PropertyValueFactory<Libro, java.sql.Date>("releaseDate"));
+        this.column_available.setCellValueFactory(new PropertyValueFactory<Libro, String>("available"));
 
         Libros = ConexionLibros.getDataBook();
         tbw_libros.setItems(Libros);
